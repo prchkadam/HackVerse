db12ae3b7caa6d220b36c8b6f7ae518340be44bd
prchkadam <divyakadam207@gmail.com>
2026-06-26 09:58:10 +0530

    Images Fixed

--- src/app/explore.tsx
+++ src/app/explore.tsx
@@ -80,10 +80,7 @@ export default function TabTwoScreen() {
                 press <ThemedText type="smallBold">w</ThemedText> in the terminal running this
                 project.
               </ThemedText>
-              <Image
-                source={require('@/assets/images/tutorial-web.png')}
-                style={styles.imageTutorial}
-              />
+          
             </ThemedView>
           </Collapsible>
 
@@ -93,7 +90,7 @@ export default function TabTwoScreen() {
               <ThemedText type="code">@3x</ThemedText> suffixes to provide files for different
               screen densities.
             </ThemedText>
-            <Image source={require('@/assets/images/react-logo.png')} style={styles.imageReact} />
+        
             <ExternalLink href="https://reactnative.dev/docs/images">
               <ThemedText type="linkPrimary">Learn more</ThemedText>
             </ExternalLink>
